edd3fe74553d529c2be0ef688c48a171eae220f3
Berlocidas <vaitiekunas.mantas@gmail.com>
2025-07-28 10:12:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import os
 import requests
 from telegram import Bot
@@ -9,16 +10,6 @@ CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 BYBIT_API_KEY = os.getenv("BYBIT_API_KEY")
 BYBIT_API_SECRET = os.getenv("BYBIT_API_SECRET")
 
+# Pavyzdinė žinutė
 bot = Bot(token=TOKEN)
-client = bybit(test=False, api_key=BYBIT_API_KEY, api_secret=BYBIT_API_SECRET)
-
-def get_price(symbol="ETHUSDT"):
-    return float(client.Market.Market_symbolInfo(symbol=symbol).result()[0]["last_price"])
-
-def send_signal():
-    price = get_price()
-    message = f"ETH/USDT kaina šiuo metu: {price}"
-    bot.send_message(chat_id=CHAT_ID, text=message)
-
-if __name__ == "__main__":
-    send_signal()
+bot.send_message(chat_id=CHAT_ID, text="Botas paleistas sėkmingai!")
